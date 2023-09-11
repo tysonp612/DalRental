f09fe4e9a16a74939c770b9fb174770b1916d79f
email <phamson061297@gmail.com>
2023-09-11 12:50:22 -0300

    Added functionality to findID for any controller

--- src/main/java/com/example/dalrental/controllers/SignUpController.java
+++ src/main/java/com/example/dalrental/controllers/SignUpController.java
@@ -1,9 +1,7 @@
 package com.example.dalrental.controllers;
+
 import javafx.fxml.FXML;
-import javafx.scene.control.Button;
-import javafx.scene.control.PasswordField;
-import javafx.scene.control.TextField;
-import javafx.scene.control.Label;
+import javafx.scene.control.*;
 import javafx.scene.input.KeyEvent;
 import models.UserModel;
 
@@ -26,80 +24,94 @@ public class SignUpController {
     private Button closeErrorPanelBtn;
     @FXML
     private Button createUserBtn;
+
     @FXML
-    private void checkLength(KeyEvent e){
-        //How to get ID of any element
-        //1. get source of event, this will return an object
-        Object source = e.getSource();
-        //2. Casting from object to textField (or the elememnt's proper type)
-        TextField input = (TextField) source;
-        //3. In the element type (no object), we can extract id by using .getId();
-        String id = input.getId();
-        if(id.equals("userInput")){
-            if(usernameInput.getText().length() <8 && usernameInput.getText().length()>2 ){
+    private void checkLength(KeyEvent e) {
+//        //How to get ID of any element
+//        //1. get source of event, this will return an object
+//        Object source = e.getSource();
+//        //2. Casting from object to textField (or the elememnt's proper type)
+//        TextField input = (TextField) source;
+//        //3. In the element type (no object), we can extract id by using .getId();
+//        String id = input.getId();
+        String id = findId(e, TextField.class);
+        if (id.equals("userInput")) {
+            if (usernameInput.getText().length() < 8 && usernameInput.getText().length() > 2) {
                 closeErrorPanel();
                 this.username = usernameInput.getText();
-            }else{
+            } else {
                 signUpErrorHandler("Username has to be in range of 2 to 8 character");
             }
         } else if (id.equals("passwordInput")) {
-            if(passwordInput.getText().length() <15 && passwordInput.getText().length()>5 ){
+            if (passwordInput.getText().length() < 15 && passwordInput.getText().length() > 5) {
                 closeErrorPanel();
                 this.password = passwordInput.getText();
-            }else{
+            } else {
                 signUpErrorHandler("Password has to be in range of 5 to 15 character");
             }
         }
 
     }
+
     @FXML
-    private void checkPasswordConfirm(){
+    private void checkPasswordConfirm() {
         //if the passwordConfirm field is not yet typed, no error shown
-        if(this.passwordConfirmInput.getText().length() > 0){
+        if (this.passwordConfirmInput.getText().length() > 0) {
             //if it is typed, start comparing with the password typed after checking length;
-            if(this.password != null && passwordConfirmInput.getText().equals(this.password)){
+            if (this.password != null && passwordConfirmInput.getText().equals(this.password)) {
                 closeErrorPanel();
                 this.confirmPassword = passwordConfirmInput.getText();
-            }else{
+            } else {
                 signUpErrorHandler("Password confirm does not match with password");
             }
         }
     }
+
     @FXML
-    private void checkDalEmail(){
-        if(this.emailInput.getText().length()>0){
-            if(this.emailInput.getText().contains("@dal")){
+    private void checkDalEmail() {
+        if (this.emailInput.getText().length() > 0) {
+            if (this.emailInput.getText().contains("@dal")) {
                 closeErrorPanel();
                 this.email = emailInput.getText();
-            }else{
+            } else {
                 signUpErrorHandler("Email has to be a Dalhousie email");
             }
         }
     }
+
     @FXML
-    private void signUpErrorHandler(String message){
+    private void signUpErrorHandler(String message) {
         signUpErrorPanel.setOpacity(1.0);
         closeErrorPanelBtn.setOpacity(1.0);
         signUpErrorPanel.setText(message);
     }
+
     @FXML
-    private void closeErrorPanel(){
+    private void closeErrorPanel() {
         signUpErrorPanel.setOpacity(0.0);
         closeErrorPanelBtn.setOpacity(0.0);
         signUpErrorPanel.setText("");
     }
 
     @FXML
-    private void createUserAndSaveToDB(){
+    private void createUserAndSaveToDB() {
         //hash password first
         //create new user
-        UserModel newUser = new UserModel(this.username,this.password,this.email);
+        UserModel newUser = new UserModel(this.username, this.password, this.email);
         //newUser.saveToDB();
-        System.out.println(newUser.toString());
+        System.out.println(newUser);
     }
-    // !!!ASK RAPHAEL FOR GENERIC OBJECT PARAMETER FOR TEXTFIELD, BUTTON, ETC... AND HOW TO USE : ?
-//    private String findId(KeyEvent event, Object object){
-//        return (Object)event.getSource().getId();
-//
-//    }
+
+
+    private <T extends Control> String findId(KeyEvent event  , Class<T> controlClass) {
+        Object source = event.getSource();
+
+        if (controlClass.isInstance(source)) {
+            T control = controlClass.cast(source);
+            return control.getId();
+        } else {
+            // Handle the case where the source is not of type T
+            return null; // or throw an exception, depending on your use case
+        }
+    }
 }
