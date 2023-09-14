f73299700f1b72e0ae038a23b5e887bf569e5137
email <phamson061297@gmail.com>
2023-09-13 23:30:59 -0300

    fix UI for login page, and connect login page fxml to MainController - sence changer, plan out note for loginController(hello-view)

---
+++ src/main/java/com/example/dalrental/controllers/LoginController.java
@@ -0,0 +1,14 @@
+package com.example.dalrental.controllers;
+
+public class LoginController {
+
+    //Load credentials (username and password - decrypted)
+
+    //Check credentials with the DB
+
+    //If error, error handling the message
+
+    //If pass, get to the main page
+
+    //Go to sign up
+}
